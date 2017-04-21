138dea816509c2496261e23bc55b3c5dc75cff66
postolenko <denis.postolenko@gmail.com>
2017-04-21 19:08:55 +0300

    Trans Rating

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
 
-    //   preload
-    // $(window).on('load', function () {
-
-    //     setTimeout(function() {
-
-    //         $(".preload-bg").fadeOut(500);
-
-    //     }, 700);
-  
-
-    // });
+    
+    var footerPosition;
 
 
     $(window).resize(function() {
 
 
-        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+        footerPosition = setTimeout(function() {
 
-        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+            clearTimeout(footerPosition);
+
+            $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+            $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+        });
 
         // -------------------------
 
@@ -32,9 +29,13 @@ $(document).ready(function() {
 
     $(function() {
 
-        $(".wrapper").css({"min-height" : $(window).height() + "px"});
+        setTimeout(function() {
 
-        $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+            $(".wrapper").css({"min-height" : $(window).height() + "px"});
+
+            $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+        });
 
     });
 
@@ -44,6 +45,34 @@ $(document).ready(function() {
                                 "border-top": ( $(".green-box").outerHeight() / 2 ) + "px solid transparent",
                                 "border-bottom": ( $(".green-box").outerHeight() / 2 ) + "px solid transparent"
                             });
+
+
+    // ----------------------------------------
+
+    var choiseListCount =  $(".choise-list").length;
+
+    var choiseListIndex = 0;
+
+    var choiseLinkCount;
+
+    var choiseLinkIndex;
+
+    for ( choiseListIndex = 0; choiseListIndex < choiseListCount; choiseListIndex++ ) {
+
+        choiseLinkCount = $(".choise-list:eq("+ choiseListIndex +") li").length;
+
+        for( choiseLinkIndex = 0; choiseLinkIndex < choiseLinkCount; choiseLinkIndex++) {
+
+            $(".choise-list:eq("+ choiseListIndex +") li a:eq("+ choiseLinkIndex +")").append("<span class='link-triangle'></span>");
+
+            $(".choise-list:eq("+ choiseListIndex +") li a:eq("+ choiseLinkIndex +") .link-triangle").css({
+                "border-top": ( $(".choise-list:eq("+ choiseListIndex +") li:eq("+ choiseLinkIndex +")").height() / 2 ) + "px solid transparent",
+                "border-bottom": ( $(".choise-list:eq("+ choiseListIndex +") li:eq("+ choiseLinkIndex +")").height() / 2 ) + "px solid transparent"
+            });
+
+        }
+
+    }
 
 
 });
